b10817f1e511ac6f3d978c1ca378d918ce57f0a4
RafaelPinhoDev <rafael.pinhodev@gmail.com>
2026-09-22 15:17:40 -0300

    Criação das classes Jogo e Personagem

---
+++ ResidentEvil/src/packageResidentEvil/Jogo.java
@@ -0,0 +1,23 @@
+package packageResidentEvil;
+
+public class Jogo {
+    private String titulo;
+    private int anoLancamento;
+    
+    
+    public String getTitulo(){
+        return this.titulo;
+    }
+    
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    
+    public int getAnoLancamento(){
+        return this.anoLancamento;
+    }
+    
+    public void setAnoLancamento(int anoLancamento){
+        this.anoLancamento = anoLancamento;
+    }
+}
---
+++ ResidentEvil/src/packageResidentEvil/Personagem.java
@@ -0,0 +1,24 @@
+package packageResidentEvil;
+
+
+public class Personagem {
+   private String nome;
+   private double vida;
+   
+   public String getNome(){
+       return this.nome;
+   }
+   
+   public void setNome(String nome){
+       this.nome = nome;
+   }
+   
+   public double getVida(){
+       return this.vida;
+   }
+   
+   public void setVida(double vida){
+       this.vida = vida;
+   }
+   
+}
